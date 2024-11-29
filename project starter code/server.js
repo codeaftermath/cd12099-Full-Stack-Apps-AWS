056e29d2bcdb6b6a3453b1ea3b209452b7497fa6
Michael Ly <17331561+codeaftermath@users.noreply.github.com>
2024-11-29 12:39:40 -0800

    Initial implementation of base requirements

--- project starter code/server.js
+++ project starter code/server.js
@@ -1,45 +1,60 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
+import express from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util.js";
 
+// Init the Express application
+const app = express();
 
+// Set the network port
+const port = process.env.PORT || 8082;
 
-  // Init the Express application
-  const app = express();
+// Use the body parser middleware for post requests
+app.use(bodyParser.json());
 
-  // Set the network port
-  const port = process.env.PORT || 8082;
-  
-  // Use the body parser middleware for post requests
-  app.use(bodyParser.json());
+app.get("/filteredimage", (req, res) => {
+  let resource_url;
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  try {
+    // parse and validate image_url
+    const image_url = new URL(req.query.image_url);
+    resource_url = image_url.href;
+  } catch (err) {
+    console.error(err);
+    res.status(400).send(`Invalid image_url {${req.query.image_url}}`);
+  }
 
-    /**************************************************************************** */
+  // check file extension of image_url target resource
+  const ext = resource_url.split(".").pop();
+  if (["png", "jpg"].findIndex((el) => el === ext.trim()) == -1) {
+    console.error(`Target resource does not contain correct file extension {${ext}}`)
+    res.status(400).send(`Invalid image file extension ${ext}`);
+    return;
+  }
 
-  //! END @TODO1
-  
-  // Root Endpoint
-  // Displays a simple message to the user
-  app.get( "/", async (req, res) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  filterImageFromURL(resource_url)
+    .then((outpath) => {
+      console.log(`Sending filtered image at {${outpath}}`)
+      res.sendFile(outpath, () => {
+        console.log(`Deleting image file at {${outpath}}`)
+        deleteLocalFiles([outpath]);
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res
+        .status(404)
+        .send(`Unable to retreive image at image_url {${resource_url}}`);
+    });
+});
 
-  // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+// Root Endpoint
+// Displays a simple message to the user
+app.get("/", async (req, res) => {
+  res.send("try GET /filteredimage?image_url={{}}");
+});
+
+// Start the Server
+app.listen(port, () => {
+  console.log(`server running http://localhost:${port}`);
+  console.log(`press CTRL+C to stop server`);
+});
